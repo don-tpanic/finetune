466fcbfe07d96547b2cd1479a7988afca49a4e3c
Xiaoliang (Ken) Luo <ken@love21.pals.ucl.ac.uk>
2022-02-22 16:19:43 +0000

    update code: fix lowAttn where intermediate_input needs to be True for finetune - required by data_loader

--- models.py
+++ models.py
@@ -31,12 +31,21 @@ def model_base(
     -------
         config_version: will load the following:
             model_name: vgg16 / vgg19 / resnet50
-            layer: where to intercept representations.
+
+            layer: where to intercept representations 
+            (NOTE the first attn layer will be applied to the output of this layer,
+            if there is only one attn layer, it is the same as the last attn layer.)
+
             input_shape=(224, 224, 3),
+
             actv_func: prediction layer only, default `sigmoid`
+
             lr: default `3e-5`
+
             train: finetune / finetune-with-lowAttn
+
             stimulus_set: 1-6
+
             intermediate_input: whether to use intercepted layer as input.
                 if False, we load the entire model regardless of `train`.
 
@@ -64,7 +73,11 @@ def model_base(
     """
     config = load_config(config_version)
     model_name = config['model_name']
-    layer = config['layer']
+
+    # TODO: better naming?
+    layer_begin = config['attn_positions'].split(',')[0]
+    layer_end = config['attn_positions'].split(',')[-1]
+    
     actv_func = config['actv_func']
     lr = config['lr']
     train = config['train']
@@ -77,7 +90,6 @@ def model_base(
             input_shape=input_shape
         )
         preprocess_func = tf.keras.applications.vgg16.preprocess_input
-        layer_reprs = model.get_layer(layer).output
 
     elif model_name == 'vgg19':
         model = tf.keras.applications.VGG19(
@@ -86,7 +98,6 @@ def model_base(
             input_shape=input_shape
         )
         preprocess_func = tf.keras.applications.vgg19.preprocess_input
-        layer_reprs = model.get_layer(layer).output
     
     elif model_name == 'resnet50':
         model = tf.keras.applications.ResNet50(
@@ -95,59 +106,56 @@ def model_base(
             input_shape=input_shape
         )
         preprocess_func = tf.keras.applications.resnet.preprocess_input
-        layer_reprs = model.get_layer(layer).output
 
-    # if we intercept early, we need to flatten the conv block
-    # and we the flattened reprs as inputs.
-    if layer not in ['flatten', 'fc1', 'fc2']:
-        layer_reprs = layers.Flatten()(layer_reprs)
 
-    # --- return model based on whether fine tune ---
-    if train == 'none':
-        model = Model(inputs=model.input, outputs=layer_reprs)
-        model.summary()
-        reprs_dims = model.get_layer(layer).output.shape[-1]
-        print(f'[Check] Cut off layer size:{reprs_dims}')
-        return model, reprs_dims, preprocess_func
+    # ----------------------------------------------------------------------
+    # ---- build up models depending on scenarios ----
+    input_shape = model.input.shape[1:]
 
-    # ------- stuff to do with the train task -------
     if stimulus_set not in ['6', 6]:
         pred_layer_units = 3
     else:
         pred_layer_units = 4
 
     # instantiate final layer
-    pred_layer = layers.Dense(
+    PredLayer = layers.Dense(
         pred_layer_units, 
         activation=actv_func, 
         name='pred'
     )
 
-    # Entire model (from input layer to pred layer)
-    if intermediate_input is False:
-        output = pred_layer(layer_reprs)
-        model = Model(inputs=model.input, outputs=output)
+    layer_begin_reprs = model.get_layer(layer_begin).output
+    if layer_begin not in ['flatten', 'fc1', 'fc2']:
+        original_layer_begin_shape = layer_begin_reprs.shape[1:]
+        layer_begin_reprs = layers.Flatten()(layer_begin_reprs)
 
-    # Intercepted model (from target layer to pred layer)
-    else:
-        intermediate_input = layers.Input(shape=layer_reprs.shape[-1])
-        output = pred_layer(intermediate_input)
-        model = Model(inputs=intermediate_input, outputs=output)
+    # The model is used to produce data (X, Y)
+    if train == 'none':
+        model = Model(inputs=model.input, outputs=layer_begin_reprs)
+        model.summary()
+        reprs_dims = model.get_layer(layer_begin).output.shape[-1]
+        print(f'[Check] Cut off layer size:{reprs_dims}')
+        return model, reprs_dims, preprocess_func
 
-    # Only finetune the output layer.
-    if train == 'finetune':
-        for layer in model.layers:
-            if layer.name in ['pred']:
-                continue
-            else:
-                layer.trainable = False
+    # NOTE: For backward compatibility
+    # (where layer_begin directly connects to pred)
+    elif train == 'finetune':
+        if intermediate_input is False:
+            output = PredLayer(layer_begin_reprs)  # flattened.
+            model = Model(inputs=model.input, outputs=output)
+        else:
+            intermediate_input = layers.Input(
+                shape=layer_begin_reprs.shape[-1]
+            )
+            output = PredLayer(intermediate_input)
+            model = Model(inputs=intermediate_input, outputs=output)
 
-    # Also finetune attn layers.
+    # ----------------------------------------------------------------------
+    # New integration with attn layers.
     elif train == 'finetune-with-lowAttn':
-        # attn layer positions
-        attn_positions = config['attn_positions'].split(',')
         
         # attn layer settings
+        attn_positions = config['attn_positions'].split(',')
         if config['attn_initializer'] == 'ones':
             attn_initializer = tf.keras.initializers.Ones()
         elif config['attn_initializer'] == 'ones-withNoise':
@@ -165,49 +173,111 @@ def model_base(
         else:
             attn_regularizer = None
 
-        # loop thru all layers and apply attn at positions.
-        dcnn_layers = model.layers[1:]
-        x = model.input
-        fake_inputs = []
-        for layer in dcnn_layers:
+        if intermediate_input is False:
+            dcnn_layers = model.layers[1:]
+            x = model.input
+            fake_inputs = []
 
-            # regardless of attn
-            # apply one layer at a time from DCNN.
-            if layer.name not in ['pred']:
+            for layer in dcnn_layers:
+
                 layer.trainable = False
-            x = layer(x)
+                x = layer(x)
 
-            # apply attn at the output of the above layer output
-            if layer.name in attn_positions:
-                attn_size = x.shape[-1]
+                # apply attn at the output of the above layer output
+                if layer.name in attn_positions:
+                    attn_size = x.shape[-1]
 
-                fake_input = layers.Input(
-                    shape=(attn_size,),
-                    name=f'fake_input_{layer.name}'
-                )
-                fake_inputs.append(fake_input)
+                    fake_input = layers.Input(
+                        shape=(attn_size,),
+                        name=f'fake_input_{layer.name}'
+                    )
+                    fake_inputs.append(fake_input)
+                    
+                    attn_weights = AttnFactory(
+                        output_dim=attn_size, 
+                        input_shape=fake_input.shape,
+                        name=f'attn_factory_{layer.name}',
+                        initializer=attn_initializer,
+                        constraint=low_attn_constraint,
+                        regularizer=attn_regularizer
+                    )(fake_input)
+
+                    # reshape attn to be compatible.
+                    attn_weights = layers.Reshape(
+                        target_shape=(1, 1, attn_weights.shape[-1]),
+                        name=f'reshape_attn_{layer.name}')(attn_weights)
+
+                    # apply attn to prev layer output
+                    x = layers.Multiply(name=f'post_attn_actv_{layer.name}')([x, attn_weights])
+
+                    # The last attn layer will be connected to the final layer `PredLayer`
+                    if layer.name == layer_end:
+                        x = layers.Flatten()(x)
+                        x = PredLayer(x)
+                        break
+
+            inputs = [model.inputs]
+
+        else:
+            intermediate_input = layers.Input(
+                shape=layer_begin_reprs.shape[-1]
+            )
+
+            # NOTE(ken) hacky
+            x = layers.Reshape(
+                target_shape=original_layer_begin_shape
+            )(intermediate_input)
+
+            dcnn_layers = model.layers[1:]
+            fake_inputs = []
+
+            ignore_layer = True
+            for layer in dcnn_layers:
                 
-                attn_weights = AttnFactory(
-                    output_dim=attn_size, 
-                    input_shape=fake_input.shape,
-                    name=f'attn_factory_{layer.name}',
-                    initializer=attn_initializer,
-                    constraint=low_attn_constraint,
-                    regularizer=attn_regularizer
-                )(fake_input)
+                if layer.name != layer_begin and ignore_layer:
+                    continue
+                else:
+                    ignore_layer = False  # permanently set flag for the rest.
+                    layer.trainable = False
 
-                # reshape attn to be compatible.
-                attn_weights = layers.Reshape(
-                    target_shape=(1, 1, attn_weights.shape[-1]),
-                    name=f'reshape_attn_{layer.name}')(attn_weights)
+                    if layer.name != layer_begin:
+                        x = layer(x)
 
-                # apply attn to prev layer output
-                x = layers.Multiply(name=f'post_attn_actv_{layer.name}')([x, attn_weights])
+                    if layer.name in attn_positions:
+                        
+                        attn_size = x.shape[-1]
 
-    input_shape = model.input.shape[1:]
+                        fake_input = layers.Input(
+                            shape=(attn_size,),
+                            name=f'fake_input_{layer.name}'
+                        )
+                        fake_inputs.append(fake_input)
+                        
+                        attn_weights = AttnFactory(
+                            output_dim=attn_size, 
+                            input_shape=fake_input.shape,
+                            name=f'attn_factory_{layer.name}',
+                            initializer=attn_initializer,
+                            constraint=low_attn_constraint,
+                            regularizer=attn_regularizer
+                        )(fake_input)
 
-    if train == 'finetune-with-lowAttn':
-        inputs = [model.inputs]
+                        # reshape attn to be compatible.
+                        attn_weights = layers.Reshape(
+                            target_shape=(1, 1, attn_weights.shape[-1]),
+                            name=f'reshape_attn_{layer.name}')(attn_weights)
+
+                        # apply attn to prev layer output
+                        x = layers.Multiply(name=f'post_attn_actv_{layer.name}')([x, attn_weights])
+
+                        # The last attn layer will be connected to the final layer `PredLayer`
+                        if layer.name == layer_end:
+                            x = layers.Flatten()(x)
+                            x = PredLayer(x)
+                            break
+
+            inputs = [intermediate_input]
+
         inputs.extend(fake_inputs)
         model = Model(inputs=inputs, outputs=x, name='dcnn_model')
 
@@ -222,7 +292,8 @@ if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"]= "-1"
 
     model, _, _ = model_base(
-        config_version='config_t1.vgg16.block4_pool.None.run1-with-lowAttn'
+        config_version='config_t1.vgg16.block4_pool.None.run1-with-lowAttn',
+        intermediate_input=True
     )
     model.summary()
-    plot_model(model, to_file='dcnn_with_lowAttn.png')
+    plot_model(model, to_file='dcnn_model.png')
