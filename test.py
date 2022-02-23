7cc5764b301c6d64244c816c1da26f4f0b85c623
Xiaoliang (Ken) Luo <ken@love22.pals.ucl.ac.uk>
2022-02-23 15:34:59 +0000

    add code: test correlation of pred weightso ver runs - to get a sense of how much variation is there due to unseeded Dense layer

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import numpy as np
 import pickle
 
 
-tests = ['000']
+tests = ['000', '001', '010', '011',
+        '100', '101', '110', '111', None]
 for test in tests:
 
-    dcnn_config_version1 = f't1.vgg16.block4_pool.{test}.run1'
-    dcnn_config_version2 = f't1.vgg16.block4_pool.{test}.run1-with-lowAttn'
+    dcnn_config_version1 = f't1.vgg16.block4_pool.{test}.run3'
+    dcnn_config_version2 = f't1.vgg16.block4_pool.{test}.run4'
 
     dcnn_save_path1 = f'results/vgg16/config_{dcnn_config_version1}/trained_weights'
     with open(os.path.join(dcnn_save_path1, 'pred_weights.pkl'), 'rb') as f:
