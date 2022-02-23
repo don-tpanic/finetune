aee7223ca75fe302cf145466d82500552e3c134c
Xiaoliang (Ken) Luo <ken@love22.pals.ucl.ac.uk>
2022-02-23 21:14:19 +0000

    attn layer unfrozen

--- layers.py
+++ layers.py
@@ -51,7 +51,7 @@ class AttnFactory(Layer):
             initializer=self.initializer,
             constraint=self.constraint,
             regularizer=self.regularizer,
-            trainable=False)
+            trainable=True)
         super(AttnFactory, self).build(input_shape)
 
     def call(self, x):
