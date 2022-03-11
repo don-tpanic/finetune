627696e9212df034dbb907bf5489cf865c1d1458
Xiaoliang (Ken) Luo <ken@love21.pals.ucl.ac.uk>
2022-03-11 18:49:06 +0000

    add code: plot zero percent attn weights after finetune across L1 and lr

--- test.py
+++ test.py
@@ -8,6 +8,73 @@ import pickle
 import matplotlib.pyplot as plt
 from utils import load_config
 
+"""
+Some functions evaluating the finetuned lowAttn weights.
+"""
+
+def between_zero_percent_and_hyperparams():
+    """
+    Plot the relationship between zeroed out 
+    attention weights after finetuning with L1 strength
+    and learning rates (two hyper-params during finetune-lowAttn)
+    """
+    runs = range(1, 33)
+    all_lr_finetune = []
+    all_reg_strength = []
+    all_zero_percent = []
+    for run in runs:
+        config_version = f'config_t1.vgg16.block4_pool.None.run{run}-with-lowAttn'
+        config = load_config(config_version)
+        lr_finetune = config['lr_finetune']
+        reg_strength = config['reg_strength']
+        dcnn_base = config['dcnn_base']
+        all_lr_finetune.append(lr_finetune)
+        all_reg_strength.append(reg_strength)
+
+        path = f'results/{dcnn_base}/{config_version}/trained_weights'
+        attn_position = config['low_attn_positions'].split(',')[0]
+        attn_weights = np.load(f'{path}/attn_weights.npy', allow_pickle=True)
+        layer_attn_weights = attn_weights.ravel()[0][attn_position]
+        zero_percent = 1 - (
+            len(np.nonzero(layer_attn_weights)[0]) / len(layer_attn_weights)
+        )
+        all_zero_percent.append(zero_percent)
+    
+    # plot a 2D scatter of x=l1 strength, y=lr rate, value=zero_percent
+    fig, ax = plt.subplots(2, 1)
+    # sort for plotting
+    # l1
+    order = np.argsort(all_reg_strength)
+    all_reg_strength = np.array(all_reg_strength)[order]
+    ax[0].plot(np.array(all_zero_percent)[order])
+    ax[0].set_xlabel('L1 strength')
+    ax[0].set_ylabel('percentage zero')
+    ax[0].set_xticks(np.arange(len(all_reg_strength)))
+    all_reg_strength[np.where(all_reg_strength == 0)[0]] = 1e-16  # avoid div by 0.
+    ax[0].set_xticklabels(
+        np.round(
+            np.log(all_reg_strength), 1
+        ), rotation='90'
+    )
+
+    # lr
+    order = np.argsort(all_lr_finetune)
+    all_lr_finetune = np.array(all_lr_finetune)[order]
+    ax[1].plot(np.array(all_zero_percent)[order])
+    ax[1].set_xlabel('learning rate')
+    ax[1].set_ylabel('percentage zero')
+    ax[1].set_xticks(np.arange(len(all_lr_finetune)))
+    ax[1].set_xticklabels(all_lr_finetune)
+    ax[1].set_xticklabels(
+        np.round(
+            np.log(all_lr_finetune), 1
+        ), rotation='90'
+    )
+
+    plt.tight_layout()
+    plt.savefig('zero_percent_l1_n_lr.png')
+
+
 
 def finetuned_lowAttn(config_version):
     config = load_config(config_version=config_version)
@@ -33,7 +100,6 @@ def finetuned_lowAttn(config_version):
         plt.savefig('attn_hist.png')
 
 
-
 def pred_stability():
     tests = ['000', '001', '010', '011',
             '100', '101', '110', '111', None]
@@ -57,5 +123,5 @@ def pred_stability():
 
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"]= f"0"
-    config_version = 'config_t1.vgg16.block4_pool.None.run12-with-lowAttn'
-    finetuned_lowAttn(config_version)
+    between_zero_percent_and_hyperparams()
+
